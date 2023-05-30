a3bf24f299d2e20b791ff60d5da6f1f995a24409
MarieGestel <marie.egestel@gmail.com>
2023-05-30 16:07:45 +0200

    transfom csv to dict

--- API.py
+++ API.py
@@ -1,23 +1,18 @@
 # -*- coding: utf-8 -*-
 from flask import Flask, jsonify, render_template
+from csv import DictReader
 import csv,json
 
 api = Flask(__name__)
 
 @api.route("/")
 def tv_news():
-          
-        """dictionnaire = {
-              'type': 'Prévision de température',
-                'valeurs': [24, 24, 25, 26, 27, 28],
-                'unite': "degrés Celcius"
-        }
-        return jsonify(dictionnaire)
-        """
+        with open("drought-tv-news.csv", 'r') as f:
+              dict_reader = DictReader(f)
+              list_of_dict = list(dict_reader)
+        return (jsonify(list_of_dict))
+             
         #return render_template("API.html")
-        tvjson = json.dumps(list(csv.reader(open("drought-tv-news.csv"))))
-        return (jsonify(tvjson))
-        
 
 if __name__ == "__main__":
     api.run(debug=True)
