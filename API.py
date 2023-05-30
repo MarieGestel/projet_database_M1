4738a878cf08530f4906870f8faecf205440f2af
MarieGestel <marie.egestel@gmail.com>
2023-05-30 14:54:09 +0200

    ajout d'une fonction json

--- API.py
+++ API.py
@@ -1,11 +1,17 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, render_template
+from flask import Flask, jsonify, render_template
 
 api = Flask(__name__)
 
 @api.route("/")
-def hello():
-        return render_template("API.html")
+def tv_news():
+        dictionnaire = {
+              'type': 'Prévision de température',
+                'valeurs': [24, 24, 25, 26, 27, 28],
+                'unite': "degrés Celcius"
+        }
+        return jsonify(dictionnaire)
+        #return render_template("API.html")
 
 if __name__ == "__main__":
     api.run(debug=True)
