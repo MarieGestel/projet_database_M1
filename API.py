652fba6b3f02c206cecf8d23ae7762b8810377d0
Gérard Kouadio <gerardkouadio@MacBook-Air-de-Gerard.local>
2023-06-13 15:30:06 +0200

    lecture des données depuis la BDD.... et commentaire dans le readme

--- API.py
+++ API.py
@@ -6,12 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 import csv
 from flask import request
 import psycopg2
-
-      #POSTGRES_USER: user
-      #POSTGRES_DB: databasetvnews
-      #POSTGRES_PASSWORD: password
 from flask_migrate import Migrate
-
 
 
 
@@ -25,43 +20,11 @@ migrate = Migrate(api, db)
 class News(db.Model):
     __tablename__ = "tv_news"
     id = db.Column(db.Integer, primary_key=True)
-    date = db.Column(db.String(120), unique=False, nullable=False)
+    date = db.Column(db.DATETIME(50), unique=False, nullable=False)
     title = db.Column(db.String(120), unique=False, nullable=False)
     url = db.Column(db.String(500), unique=False, nullable=False)
     media = db.Column(db.String(50), unique=False, nullable=False)
 
-
-    
-    #def __repr__(self):
-     # return '<User %r>' % self.username
-
-      
-
-
-@api.route('/sauvegarder_csv', methods=['POST'])
-def sauvegarder_csv():
-    fichier = request.files['drought-tv-news.csv']
-
-    if fichier:
-        donnees = fichier.read().decode('utf-8')
-        lignes = csv.reader(donnees.splitlines(), delimiter=',')
-
-
-        for ligne in lignes:
-            nouvel_utilisateur = News(date=ligne[0], title=ligne[1], url=ligne[2], media=ligne[3])
-            db.session.add(nouvel_utilisateur)
-
-
-      
-        db.session.commit()
-
-        return 'Données CSV sauvegardées avec succès'
-    else:
-        return 'Aucun fichier CSV n\'a été envoyé'
-
-#curl -X POST -F "drought-tv-news.csv" http://localhost:8080/sauvegarder_csv
-
-    
 
 # Pour lire les données depuis une BDD à l'aide d'un API
 
@@ -79,30 +42,8 @@ def obtenir_utilisateurs():
     return (jsonify(result))
 
 
-#---
-
-
-""" @api.route("/")
-def tv_news():
-      with open("drought-tv-news.csv", 'r') as f:
-           dict_reader = DictReader(f)
-           list_of_dict = list(dict_reader)
-      return (jsonify(list_of_dict))
-             
-        #return render_template("API.html")
- """
-#--- 
-
-
 if __name__ == "__main__":
     api.cli.add_command(db.init_app(api))
     api.cli.add_command(db)
     api.run(debug=True, port=8080)
 
-#if __name__ == "__main__":
-    #api.run(debug=True)
- #   api.run(debug=True, port=8080)
-
-
-
-####
--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # projet_database_M1
+
+# Pour cloner le dossier, prendre la clé SSH
+git clone git@github.com:MarieGestel/projet_database_M1.git
+
+
+# Make sure your postgresql server is running before runing code below
+# Make sure also your Flask server is running.
+# f3623bde9430 ==> faire docker ps pour voir l'ID du container de postegresql
+docker exec -it f3623bde9430 bash 
+
+psql -U user -d mydb
+
+# Si la table n'existe pas, la créer
+CREATE DATABASE mydb; 
+
+# After that, you need to create a table
+CREATE TABLE tv_news ( id SERIAL PRIMARY KEY, date VARCHAR(120) NOT NULL, title VARCHAR(120) NOT NULL, url VARCHAR(500) NOT NULL, media VARCHAR(50) NOT NULL);
+
+# Pour voir la table
+\dt 
+
+# Pour afficher les données qui sont dans la table
+SELECT * FROM tv_news; 
+
+# Copie de mon fichier csv de mon local vers le container 
+docker cp drought-tv-news.csv f3623bde9430:/drought-tv-news.csv
+
+# Ensuite je copie les données dans la table. 
+COPY tv_news(date, title, url, media) FROM '/drought-tv-news.csv' DELIMITER ',' CSV HEADER;
+
+# S'il envoi une erreur disant que la taille d'une colonne est petite, le code ci-dessous permet de le modifier.
+ALTER TABLE tv_news ALTER COLUMN title TYPE VARCHAR(255);
+
+
+# Manipulation de Git:
+git pull
+git status
+git add nom du fichier
+git add .
+git commit -m ‘’message’’
+git push
+
+
